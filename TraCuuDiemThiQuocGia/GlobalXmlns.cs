cc1762f8d0f432cc8bbb50ed3d0225879d46c11b
HungMG <sangm1238@gmail.com>
2026-03-16 11:10:45 +0700

    Add project files.

---
+++ TraCuuDiemThiQuocGia/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TraCuuDiemThiQuocGia")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TraCuuDiemThiQuocGia.Pages")]
